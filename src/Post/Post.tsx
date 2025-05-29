7100be93a114fd2c2df16fbb1bb48dc818d7eeab
Vlad Lapusta <vlad.lapusta@bk.ru>
2025-05-29 22:03:58 +0300

    Add Post

--- src/Post/Post.tsx
+++ src/Post/Post.tsx
@@ -4,7 +4,9 @@ import React from "react";
 export const Post = () => {
     return (
         <div>
-            <h2>Post1111</h2>
+            <h2>Post1</h2>
+            <h2>Post2</h2>
+            <h2>Post3</h2>
         </div>
     )
 }
